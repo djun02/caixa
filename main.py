c21ac095ffe96e09540a7af165c7be57e4a28c49
madcode <madcode@cellep.com>
2018-10-02 17:32:16 -0300

    dispensador

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ class Cliente:
 
 
     def depositar(self, valor):
-        self.saldo += valor
-        return True
+        self.saldo += int(valor)
+        
 
     def saque(self,valor):
         valor = int(valor)
@@ -65,17 +65,50 @@ class Display:
     def box(self,txt):
         print(txt)
 
-
-
     def telaDep(self):
         i = input("qual quantidade deseja depositar?")
         return i
+
+
+class DispensadorNotas:
+    def __init__(self):
+        self.montante = 0
+        self.cedula = 100
+        self.contagem = 0
+
+    def dispensar(self,valor):
+        self.montante = int(valor)
+        while True:
+            if self.montante >= self.cedula:
+                self.montante -= self.cedula
+                self.contagem += 1
+            
+            
+            else:
+                if self.contagem > 0:
+                    print("{0} cedulas de {1}".format(self.contagem, self.cedula))
+                self.contagem = 0
+                if self.montante == 0:
+                    break
+                if self.cedula == 100:
+                    self.cedula = 50
+                elif self.cedula == 50:
+                    self.cedula = 20
+                elif self.cedula == 20:
+                    self.cedula = 5
+                elif self.cedula == 5:
+                    self.cedula = 2
+            
+            continue
+
+
 
 class CaixaEletronico:
 
     def __init__(self):
         self.cliente = Cliente()
         self.display = Display()
+        self.dispensador = DispensadorNotas()
 
     def start(self):
         while True:
@@ -91,8 +124,16 @@ class CaixaEletronico:
                         d = self.display.telaSaque()
                         if self.cliente.saque(d):
                             self.display.box("Saque realizado com sucesso")
+                            self.dispensador.cedula=100
+                            self.dispensador.dispensar(d)
                         else:
                             self.display.box("Valor inválido")
+                    elif b == 3:
+                        e = self.display.telaDep()
+                        self.cliente.depositar(e)
+                    
+                         
+                        
 
 
 
